2b388fd6676ad2a3d55f00a81a52b6ac86ec5c2c
Sourabhtripathi1 <ganeshprasadtripathi38@gmail.com>
2025-03-24 20:18:20 +0530

    24-03-2025

--- frontend/src/hooks/useInitializeTheme.js
+++ frontend/src/hooks/useInitializeTheme.js
@@ -4,16 +4,23 @@ import $ from "jquery";
 export const useInitializeTheme = () => {
   useEffect(() => {
     // 1. Preloader
-    const handleLoad = () => {
+    const hideLoader = () => {
       console.log("main.js file loaded");
-      $("#preloader-active").delay(450).fadeOut("slow");
-      $("body").delay(450).css({
+      $("#preloader-active").delay(200).fadeOut("slow");
+      $("body").delay(200).css({
         overflow: "visible",
       });
     };
 
-    // Call handleLoad when component mounts
-    handleLoad();
+    const showLoader = () => {
+      console.log("loader started");
+      $("#preloader-active").fadeIn("slow");
+      $("body").css({
+        overflow: "hidden",
+      });
+    };
+
+    hideLoader();
 
     // 8. Data Background
     $("[data-background]").each(function () {
